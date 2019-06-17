0de69d18ed815eb0d19c4fc4d33ef90a9fd2e125
Manuel Abascal <manuelsuarezabascal@gmail.com>
2019-06-17 01:43:28 -0400

    Changed component name from home to forecast

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home container animated fadeIn delay-4">
+  <div class="forecast container animated fadeIn delay-4">
     <h1>{{ msg }}</h1>
-    <div class="home app-title my-3">
+    <div class="forecast app-title my-3">
       <p class="m-2">A weather app built <a class="vuejs-link text-decoration-none" href="https://vuejs.org/" target="_blank" rel="noopener">Vuejs</a> & <a class="open-weather-api-link text-decoration-none" href="https://openweathermap.org/" target="_blank" rel="noopener">Open Weather App.</a></p>
 
       <p class="m-2">Made with &#10084;&#65039; by <a class="github-profile-link color-info text-decoration-none" href="https://github.com/Manuel-Suarez-Abascal" target="_blank" rel="noopener">Manuel Abascal</a></p>
@@ -38,7 +38,7 @@
 import axios from "axios"
 
 export default {
-  name: "Home",
+  name: "Forecast",
   props: {
     msg: String,
   },
